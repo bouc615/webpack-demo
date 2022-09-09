cea97c0a48a577bf043b2b79e18e18a0acc89b1d
bouc615 <bouc_frontend@163.com>
2022-09-10 00:23:04 +0800

    webpack搭建vue3项目

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    hello, {{name}}
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {ref} from 'vue'
+  const name = ref<string>('bouc')
+
+  console.log('name:', name)
+</script>
